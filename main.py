48bb5d7edf33fd86cbcd215f7fe369a104d52630
r-elen <elen.ryazanova@gmail.com>
2022-05-28 12:43:20 +0300

    28.05 Зачетное задание: Генерация списка 100 словарей случайных книг c указанием названия, года, кол-ва страниц...

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
-import random
-
 from faker import Faker
+import random
 import json
 
 from conf import MODEL
 
 
-def pk_gen():
-    input_ = int(input("введите чило: "))
-    yield input_
-    input_ += 1
+def random_title(book_name: str) -> str:
+    """
+    Получение случайного названия книги
+
+    :param book_name: Название файла с книгами
+    :return: Название случайной книги из списка
+    """
+    with open(book_name, 'r', encoding='utf8') as f:
+        list_books = []
+        for line in f:
+            list_books.append(line.rstrip('\n'))
+    return random.choice(list_books)
 
 
-def random_title():
-    with open('book.txt', 'r', encoding='utf8') as f:
-        title = random.choice(f.readlines())
-    return title
+def random_book_num() -> str:
+    """
+    Получение международного стандартного книжного номера. Генерируется случайным образом с помощью модуля Faker
 
-
-def random_book_num():
+    :return: Книжный номер
+    """
     fake_ru = Faker('ru_RU')
     return fake_ru.isbn13()
 
 
-def random_author():
+def random_author() -> list[str]:
+    """
+    Получение списка от 1 до 3 ФИО авторов. Генерируется случайным образом с помощью модуля Faker
+
+    :return: Cписок авторов
+    """
     fake_ru = Faker('ru_RU')
     author_count = random.randint(1, 3)
     authors = []
@@ -32,23 +43,29 @@ def random_author():
     return authors
 
 
-def main(first_num):
-    model = MODEL
-    pk = pk_gen()
-    title = random_title()
-    year = random.randint(1000, 2022)
-    pages = random.randint(1, 10000)
-    isbn13 = random_book_num()
-    rating = f"{random.uniform(0, 5):.2f}"
-    price = f"{random.uniform(10, 5000):.2f}"
-    author = random_author()
+def main(start_num: int = 1) -> None:
+    """
+    Генерация списка 100 словарей случайных книг c указанием названия, года, кол-ва страниц... .
 
+    :param start_num: Число с которого начинается нумерация
+    :return: .json файл со списком из 100 словарей случайных книг
+    """
     with open("output.json", 'w', encoding="utf8") as f:  # записать список в JSON файл
-
-        f.write(json.dumps(..., ensure_ascii=False, indent=4))
+        data_list = []
+        for i in range(start_num, start_num+100):
+            iner_data_dict = {
+                'title': random_title('book.txt'),
+                'year': random.randint(1000, 2022),
+                'pages': random.randint(1, 10000),
+                'isbn13': random_book_num(),
+                'rating': f"{random.uniform(0, 5):.2f}",
+                'price': f"{random.uniform(10, 5000):.2f}",
+                'author': random_author()
+                }
+            data = {'model': MODEL, 'pk': i, 'fields': iner_data_dict}
+            data_list.append(data)
+        f.write(json.dumps(data_list, ensure_ascii=False, indent=4))
 
 
 if __name__ == '__main__':
-    print(random_title())
-    # print(next(pk_gen()))
-    # main()
+    main()
